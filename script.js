e071cd3c81b6f288e41621fb14cc0cfa80414852
con0fav <connor.favero@gmail.com>
2020-11-14 19:45:19 -0800

    testing different local storage methods

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // use moments.js to display current date at the top of the page under jumbotron text
-// single line each for hours between 9 AM and 5 PM
-// timeblock rows can be built with bootstrap? textarea rows? text boxes? forms?
 // grey: past red: now green: future
 // timeblock rows are text where you can enter text
 // text stored in timeblock rows are saved to local storage WHEN save button on the right is pressed
@@ -8,3 +6,21 @@
 
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(time);
+
+$(".save").click(function() {
+    $(".event").html();
+    localStorage.setItem(key, value);
+})
+
+// function saveEvent() {
+//     localStorage.event = $(".event").val();
+// }
+
+// function loadEvent() {
+//     $(".event").val(localStorage.event);
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     const event = array[i];
+    
+// }
