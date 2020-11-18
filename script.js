1d266f04ec740cd51bcef6aecc66b3086b5649a2
con0fav <connor.favero@gmail.com>
2020-11-17 18:17:48 -0800

    cleaned code, testing different save functions

--- script.js
+++ script.js
@@ -7,59 +7,6 @@
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(time);
 
-var eventInput = $("#event");
-var events = [];
-
-function init() {
-  var savedEvents = JSON.parse(localStorage.getItem("events"));
-
-  if (savedEvent !== null){
-    events = savedEvents
-  }
-}
-
-
-function storeEvents() {
-  localStorage.setItem("events", JSON.stringify(events));
-}
 
 
 
-
-
-// var event = [];
-
-// init();
-
-// function init() {
-//     // Get stored todos from localStorage
-//     // Parsing the JSON string to an object
-//     var events = JSON.parse(localStorage.getItem(".event"));
-  
-//     // If todos were retrieved from localStorage, update the todos array to it
-//     if (storedTodos !== null) {
-//       todos = storedTodos;
-//     }
-
-//   }
-
-
-
-
-// $(".save").click(function() {
-//     $(".event").html();
-//     localStorage.setItem(key, value);
-// })
-
-// function saveEvent() {
-//     localStorage.event = $(".event").val();
-// }
-
-// function loadEvent() {
-//     $(".event").val(localStorage.event);
-// }
-
-// for (let i = 0; i < array.length; i++) {
-//     const event = array[i];
-    
-// }
